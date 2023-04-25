e8ae40fe32e2c7791119d8fe72a1ac4d52461630
Igor Cananea <icc@avalonbits.com>
2023-04-25 10:27:30 -0400

    Updated arduino library manager related files and deleted more code. (#3)

--- component.mk
+++ component.mk
@@ -1,5 +1,5 @@
 
-COMPONENT_ADD_INCLUDEDIRS := src src/comdrivers src/devdrivers src/dispdrivers src/emudevs src/network src/fonts src/images
-COMPONENT_SRCDIRS := src src/comdrivers src/devdrivers src/dispdrivers src/emudevs src/network src/fonts src/images
+COMPONENT_ADD_INCLUDEDIRS := src src/comdrivers src/devdrivers src/dispdrivers src/network src/fonts src/images
+COMPONENT_SRCDIRS := src src/comdrivers src/devdrivers src/dispdrivers src/network src/fonts src/images
 CXXFLAGS += -O2
 
